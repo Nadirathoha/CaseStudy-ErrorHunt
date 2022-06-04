e1775b5b0caf2861e6f78a702ca68eb04cb50268
Nadirathoha <102664938+Nadirathoha@users.noreply.github.com>
2022-06-04 19:42:56 +0530

    Initial commit

---
+++ src/routes/homerouter.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const homeRouter = express.Router();
+
+
+homeRouter.get('/',function(req,res){
+
+    res.render('home',{});
+    
+})
+
+
+
+
+
+
+
+module.exports = homeRouter;
